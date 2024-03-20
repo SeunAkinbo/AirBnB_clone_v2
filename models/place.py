b4ef26502434046611d5e99700c7db027532f7cc
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-03-20 06:50:38 -0700

    Updated

--- models/place.py
+++ models/place.py
@@ -41,8 +41,7 @@ class Place(BaseModel, Base):
     amenity_ids = []
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
-        reviews = relationship("Review",
-                               cascade='all, delete, delete-orphan',
+        reviews = relationship("Review", cascade='all, delete, delete-orphan',
                                backref="place")
 
         amenities = relationship("Amenity",
