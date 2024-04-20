0bd9322e8fcbf3c1b864d1cec9ae8724be8a1530
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-04-20 03:51:24 +0000

    Route HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,11 +11,11 @@ def hello_hbnb():
     """Hello function binding the url to the route"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Hello function binding the url to the route"""
     return "HBNB"
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
