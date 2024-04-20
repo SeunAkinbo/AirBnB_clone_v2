35685aa7e39b20427acd9151c54ccff04c82ca74
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-04-20 04:05:06 +0000

    Python Route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Python web framework module - 0-hello_route"""
+
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Hello function binding the url to the route"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Hello function binding the url to the route"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_route(text):
+    """text display route"""
+    text = text.replace("_", " ")
+    return "C {}".format(escape(text))
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def python_route(text="is cool"):
+    """Route displays python and text"""
+    text = text.replace("_", " ")
+    return "Python {}".format(escape(text))
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
