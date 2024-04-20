aa35a3e80cb780973ebd0165e1896620137d2120
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-04-20 03:37:10 +0000

    Displays C

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Python web framework module - 0-hello_route"""
+
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+	"""Hello function binding the url to the route"""
+	return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+        """Hello function binding the url to the route"""
+        return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_route(text):
+	"""text display route"""
+	text = text.replace("_", " ")
+	return "C {}".format(escape(text))
+
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=5000)
