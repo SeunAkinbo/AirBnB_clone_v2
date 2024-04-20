b9efccedea3116b7c26afa63a91f22d62ffaca99
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-04-20 02:41:57 +0000

    Pycodestyle Conformity

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-"""
-Python web framework module - 0-hello_route
-"""
-
+"""Python web framework module - 0-hello_route"""
 from flask import Flask
+
 app = Flask(__name__)
 
 
@@ -11,6 +9,7 @@ app = Flask(__name__)
 def hello():
 	"""Hello function binding the url to the route"""
 	return "Hello HBNB!"
+
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=5000)
