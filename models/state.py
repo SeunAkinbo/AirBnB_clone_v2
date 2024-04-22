c501d864130dba181f6c78cff31ad361ded54829
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-04-22 02:22:20 +0000

    Pycodestyling Compliance

--- models/state.py
+++ models/state.py
@@ -10,31 +10,28 @@ from os import getenv
 
 class State(BaseModel, Base):
     """Represents a state for a MySQL database.
-
+    
     Inherits from SQLAlchemy Base and links to the MySQL table states.
 
     Attributes:
-	__tablename__ (str): The name of the MySQL table to store States.
-	name (sqlalchemy String): The name of the State.
-	cities (sqlalchemy relationship): The State-City relationship.
+        __tablename__ (str): The name of the MySQL table to store States.
+        name (sqlalchemy String): The name of the State.
+        cities (sqlalchemy relationship): The State-City relationship.
     """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
-	cities = relationship("City", backref="state", cascade="all, delete")
+        cities = relationship("City", backref="state", cascade="all, delete")
     else:
-	@property
-	def cities(self):
-	    """Get a list of City instances with
-		state_id equals to the current State.id.
-
-	    This is a getter attribute for FileStorage
-		relationship between State and City.
-	    """
-	    city_list = []
-	    for city in models.storage.all(City).values():
-		if city.state_id == self.id:
-		    city_list.append(city)
-	    return city_list
-
+        @property
+        def cities(self):
+            """Get a list of City instances with
+            state_id equals to the current State.id.This is a getter attribute for FileStorage
+            relationship between State and City
+            """
+            city_list = []
+            for city in models.storage.all(City).values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
