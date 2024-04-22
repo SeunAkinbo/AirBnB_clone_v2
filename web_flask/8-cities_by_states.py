9abd8fc26d6c031c5436dbcc27c232879fc037f7
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-04-22 00:48:04 +0000

    Updates

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -8,11 +8,11 @@ from models import storage
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    """Renders the HTML, displaying states in a list of States"""
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
-    return render_template("7-states_list.html", states=states)
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    """Renders the HTML, displaying cities in each State"""
+    states = storage.all("State").values()
+    return render_template("8-cities_by_states.html", states=states)
 
 
 @app.teardown_appcontext
