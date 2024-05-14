92be67bb43e049138f7c339e16594358b1c94131
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-05-14 22:16:33 +0000

    Updated Files

--- web_flask/0-hello.py
+++ web_flask/0-hello.py
@@ -16,6 +16,14 @@ def start():
     """Serving content on route"""
     return "Hello HBNB!"
 
+def find_port():
+    s = socket.socket(socket.AF_INET, type=socket.SOCK_STREAM)
+    s.bind(('localhost', 0))
+    address, port = s.getsockname()
+    s.close()
+    return port
+
 
 if __name__ == "__main__":
+    port = find_port()
     app.run(host='0.0.0.0', port=port, debug=True)
